4ff4e7c83f6416799da9f86df057a10702333f10
Edward <medward@deloitte.com>
2024-12-01 00:53:38 +0200

    Remove node_modules from repository

---
+++ features/pageobjects/page.ts
@@ -0,0 +1,16 @@
+import { driver } from '@wdio/globals'
+import { Launcher } from '@wdio/cli';
+
+/**
+* main page object containing all methods, selectors and functionality
+* that is shared across all page objects
+*/
+export default class Page {
+    /**
+    * Opens a sub page of the page
+    * @param path path of the sub page (e.g. /path/to/page.html)
+    */
+    public open (path: string) {
+       driver.launchApp();
+    }
+}
